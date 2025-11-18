40f9732638dca306516b0be1dcd1df5fe6611224
IsratErin <israterin11@gmail.com>
2025-11-18 18:52:27 +0100

    Squashed commit of the following:
    commit dd8021ae87a47188655bdf6d66843ee586c5fa4c
    Author: IsratErin <israterin11@gmail.com>
    Date:   Tue Nov 18 18:48:39 2025 +0100
    
        protected route added
    
    commit e5201833bf703db8679c135eac89bae71f56b0b3
    Author: IsratErin <israterin11@gmail.com>
    Date:   Tue Nov 18 18:16:51 2025 +0100
    
        Create user and Login user thorugh firebase authentication

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,8 +1,27 @@
 import axios from "axios";
+import { getIdToken } from "../auth/authService";
 
 const api = axios.create({
   baseURL: `https://server-gametimetracker.vercel.app/`,
   withCredentials: false,
 });
+
+api.interceptors.request.use(
+  async (config) => {
+    try {
+      const token = await getIdToken();
+      if (token && config.headers) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+      localStorage.setItem("authToken", token || "");
+    } catch (error) {
+      console.error("Error retrieving ID token:", error);
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default api;
