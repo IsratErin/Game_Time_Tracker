a342cbcd2696b98003210d115a899abca161d73d
IsratErin <israterin11@gmail.com>
2025-11-18 19:12:24 +0100

    vercel confic server

--- server/src/firebase/firebaseAdmin.ts
+++ server/src/firebase/firebaseAdmin.ts
@@ -1,12 +1,7 @@
 import admin from "firebase-admin";
-import { readFile } from "fs/promises";
 
 const serviceAccount = JSON.parse(
-  (
-    await readFile(
-      new URL("../../firebaseServiceAccountKey.json", import.meta.url)
-    )
-  ).toString() // Convert Buffer to string
+  process.env.FIREBASE_SERVICE_ACCOUNT_KEY || "{}"
 );
 
 admin.initializeApp({
