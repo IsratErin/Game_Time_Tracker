ea65e48dad71aaddb124d9be9300470067fc6718
IsratErin <israterin11@gmail.com>
2025-11-19 10:44:45 +0100

    removed  / from client deployed url

--- server/index.ts
+++ server/index.ts
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 // Allow requests from both local and deployed clients
 const allowedOrigins = [
   "http://localhost:5173", // Local development
-  "https://client-gametimetracker.vercel.app/", // Deployed client
+  "https://client-gametimetracker.vercel.app", // Deployed client
 ];
 
 app.use(
