4475cb667059b88c9cae10b090dd8c47777b7590
IsratErin <israterin11@gmail.com>
2025-11-19 12:33:58 +0100

    update

--- client/src/auth/firebase.init.ts
+++ client/src/auth/firebase.init.ts
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCOeqVw9aUgI5llzWYYeKZrnLBpCIsaEyk",
-  authDomain: "gametimetracker-8a174.firebaseapp.com",
-  projectId: "gametimetracker-8a174",
-  storageBucket: "gametimetracker-8a174.firebasestorage.app",
-  messagingSenderId: "542903071488",
-  appId: "1:542903071488:web:788bb75e716581fcdd79e5",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
